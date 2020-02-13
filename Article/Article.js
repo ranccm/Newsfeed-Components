06bbad81589b66feb1e79bf691232e922b8c1a39
ranccm <ranimnapier@gmail.com>
2020-02-12 16:26:39 -0800

    working on articles.js

--- Article/Article.js
+++ Article/Article.js
@@ -102,9 +102,6 @@ const data = [
   Hint: You will need to use createElement more than once here!
 
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
-
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
   Step 3: return the entire component.
 
   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
@@ -112,3 +109,54 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+
+function createArticle(obj){
+  const article = document.createElement('div');
+  const title = document.createElement('h2');
+  const date = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3  = document.createElement('p');
+  const button = document.createElement('span');
+
+  
+  article.appendChild(title);
+  article.appendChild(date);
+  article.appendChild(p1);
+  article.appendChild(p2);
+  article.appendChild(p3);
+  article.appendChild(button);
+
+  article.classList.add('article');
+  date.classList.add('date');
+  button.classList.add('expandButton');
+
+   title.textContent = obj.title;
+  date.textContent = "date";
+  p1.textContent = "firstParagraph";
+  p2.textContent = "secondParagraph";
+  p3.textContent = "thirdParagraph";
+  button.textContent= 'button';
+
+
+  // Step 2: Add an event listener to the expandButton span.
+  // This event listener should toggle the class 'article-open' on the 'article' div.
+
+  button.addEventListener('click', event => {
+    console.log('button click', event.target);
+
+    article.classList.toggle('article-open')
+  })
+
+  return article; 
+}
+
+const body = document.querySelector('.articles');
+console.log(body);
+
+data.map(data =>{
+  body.appendChild(createArticle(data));
+})
+
+createArticle();
